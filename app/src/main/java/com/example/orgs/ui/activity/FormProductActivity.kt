45f2f76bb511f6cfc7736bf228d51e58195afb97
Patrick <patrickfbahia@hotmail.com>
2023-04-11 16:34:15 -0300

    criando nova activity

---
+++ app/src/main/java/com/example/orgs/ui/activity/FormProductActivity.kt
@@ -0,0 +1,10 @@
+package com.example.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.orgs.R
+
+class FormProductActivity :
+    AppCompatActivity(R.layout.activity_form_product) {
+
+}
