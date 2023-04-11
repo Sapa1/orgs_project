32f63d6f06d32737290ab95101bbf0e7ba3d0979
Patrick <patrickfbahia@hotmail.com>
2023-04-11 16:10:33 -0300

    first commit

---
+++ app/src/main/java/com/example/orgs/model/Product.kt
@@ -0,0 +1,9 @@
+package com.example.orgs.model
+
+import java.math.BigDecimal
+
+class Product(
+    val name: String,
+    val description: String,
+    val cost: BigDecimal,
+)
---
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.orgs.ui.activity
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.R
+import com.example.orgs.model.Product
+import com.example.orgs.ui.recyclerview.adapter.ListProductAdapter
+import java.math.BigDecimal
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        val title = findViewById<TextView>(R.id.titleCest)
+//        title.text = "Cesta de frutas"
+//        val description = findViewById<TextView>(R.id.descriptionCest)
+//        description.text = "Laranja, manga e uva"
+//        val cost = findViewById<TextView>(R.id.costItemCest)
+//        cost.text = "R$ 19.99"
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListProductAdapter(
+            context = this, products = listOf(
+                Product(
+                    name = "teste",
+                    description = "teste desc",
+                    cost = BigDecimal("19.99")
+                ),
+                Product(
+                    name = "teste2",
+                    description = "teste desc2",
+                    cost = BigDecimal("29.99")
+                ),
+                Product(
+                    name = "teste3",
+                    description = "teste desc3",
+                    cost = BigDecimal("39.99")
+                ),
+            )
+        )
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+}
