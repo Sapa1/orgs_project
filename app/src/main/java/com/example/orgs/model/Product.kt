85a798ff5048b02885774b817d2b87ad67ef7fce
Patrick <patrickfbahia@hotmail.com>
2023-04-27 11:45:26 -0300

    pegando valores de nome/desc/valor quando clico em salvar

--- app/src/main/java/com/example/orgs/model/Product.kt
+++ app/src/main/java/com/example/orgs/model/Product.kt
@@ -2,7 +2,7 @@ package com.example.orgs.model
 
 import java.math.BigDecimal
 
-class Product(
+data class Product(
     val name: String,
     val description: String,
     val cost: BigDecimal,
--- app/src/main/java/com/example/orgs/ui/activity/FormProductActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormProductActivity.kt
@@ -2,9 +2,40 @@ package com.example.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import com.example.orgs.R
+import com.example.orgs.model.Product
+import java.math.BigDecimal
 
 class FormProductActivity :
     AppCompatActivity(R.layout.activity_form_product) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val saveButton = findViewById<Button>(R.id.buttonForm)
+        saveButton.setOnClickListener {
+            val fieldTitleForm = findViewById<EditText>(R.id.titleForm)
+            val titleForm = fieldTitleForm.text.toString()
+            val fieldDescriptionForm = findViewById<EditText>(R.id.descriptionForm)
+            val descriptionForm = fieldDescriptionForm.text.toString()
+            val fieldCostForm = findViewById<EditText>(R.id.costForm)
+            val costForm = fieldCostForm.text.toString()
+            val value = if (costForm.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(costForm)
+            }
+            val newProduct = Product(
+                name = titleForm,
+                description = descriptionForm,
+                cost = value,
+            )
 
+            Log.i("FormProductActivity", "onCreate: $newProduct")
+        }
+    }
 }
+
